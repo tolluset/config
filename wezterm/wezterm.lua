d995ad48fce1908909d1398dae87b8eee2998805
tolluset <dlqud19@gmail.com>
2023-11-06 11:04:36 +0900

    chore: remove shift k clear console cuz, not work properly with tmux

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,7 +3,6 @@ return {
 	keys = {
 		-- clear screen
 		{ key = "k", mods = "CMD", action = wezterm.action({ ClearScrollback = "ScrollbackAndViewport" }) },
-		{ key = "K", mods = "SHIFT", action = wezterm.action({ ClearScrollback = "ScrollbackAndViewport" }) },
 
 		-- move by optons + ← →
 		{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
