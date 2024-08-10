b611a4e26b41cea1f4e9ef7719fa334dc4888280
tolluset <dlqud19@gmail.com>
2024-08-10 17:27:49 +0900

    feat: set console.info instead of console.warn

--- nvim/lua/plugins/log.lua
+++ nvim/lua/plugins/log.lua
@@ -5,6 +5,42 @@ return {
       require("debugprint").setup({
         display_counter = false,
         print_tag = "🚀 ",
+        filetypes = {
+          ["js"] = {
+            left = 'console.info("',
+            right = '")',
+            mid_var = '", ',
+            right_var = ")",
+          },
+
+          ["javascript"] = {
+            left = 'console.info("',
+            right = '")',
+            mid_var = '", ',
+            right_var = ")",
+          },
+
+          ["javascriptreact"] = {
+            left = 'console.info("',
+            right = '")',
+            mid_var = '", ',
+            right_var = ")",
+          },
+
+          ["typescript"] = {
+            left = 'console.info("',
+            right = '")',
+            mid_var = '", ',
+            right_var = ")",
+          },
+
+          ["typescriptreact"] = {
+            left = 'console.info("',
+            right = '")',
+            mid_var = '", ',
+            right_var = ")",
+          },
+        },
       })
     end,
     version = "*",
