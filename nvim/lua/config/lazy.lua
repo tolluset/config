ce1255fd0043882f5ca2891e62b5eafc886354e6
tolluset <dlqud19@gmail.com>
2025-09-15 20:05:52 +0900

    up

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -22,7 +22,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
-    { import = "lazyvim.plugins.extras.ai.copilot-chat" },
+    -- { import = "lazyvim.plugins.extras.ai.copilot-chat" },
 
     -- import/override with your plugins
     { import = "plugins" },
--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -7,6 +7,34 @@ return {
         rust_analyzer = {
           enabled = false,
         },
+        tailwindcss = {
+          filetypes_exclude = { "markdown" },
+          filetypes_include = { "tsx", "jsx", "typescript", "javascript" },
+        },
+      },
+      setup = {
+        tailwindcss = function(_, opts)
+          local tw = LazyVim.lsp.get_raw_config("tailwindcss")
+          opts.filetypes = opts.filetypes or {}
+
+          vim.list_extend(opts.filetypes, tw.default_config.filetypes)
+
+          opts.filetypes = vim.tbl_filter(function(ft)
+            return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
+          end, opts.filetypes)
+
+          opts.settings = {
+            tailwindCSS = {
+              includeLanguages = {
+                elixir = "html-eex",
+                eelixir = "html-eex",
+                heex = "html-eex",
+              },
+            },
+          }
+
+          vim.list_extend(opts.filetypes, opts.filetypes_include or {})
+        end,
       },
     },
   },
